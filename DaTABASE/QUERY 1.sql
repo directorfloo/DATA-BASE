8a34aacdc28725b18634512dfb04b525afae75f7
directorfloo <folorunsho891@gmail.com>
2025-11-13 04:21:19 +0100

    Add files via upload

---
+++ DaTABASE/QUERY 1.sql
@@ -0,0 +1,5 @@
+use salesdb; -- Selecting the database i want to use
+
+select *
+from customers;
+CREATE DATA
---
+++ DaTABASE/user_db.sql
@@ -0,0 +1,11 @@
+use user_db;
+
+--  CREATE TABLE users (
+--  id INT PRIMARY KEY,
+--  name VARCHAR(100),
+--  email VARCHAR(100) UNIQUE
+--  );
+ 
+ SELECT id, name, email
+ from users;
+ 
